a770a1ed6cb2087cf238c5239c9edae34412aaa6
reismgadsden <rgadsden287@gmail.com>
2022-09-23 09:19:18 -0400

    inital commit

---
+++ MetalGLS.py
@@ -0,0 +1,10 @@
+"""
+This program will explore other regression models that are offered by the
+statsmodels package.
+
+author: Reis Gadsden 2022-09-23
+modified: 2022-09-23
+class: CS-5245 @ Appalchian State University
+instructor: Dr. Mitchell Parry
+github: https://github.com/reismgadsden/MetalGLS
+"""
